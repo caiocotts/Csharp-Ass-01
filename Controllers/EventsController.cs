8071d2baa28ceaf97f96a97a3fd2bbed77cc9ec6
keaganPC <keagan.cameron@georgebrown.ca>
2025-10-05 17:54:16 -0400

    adding EventsController functionality (currently not working)

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -1,10 +1,19 @@
+using Assignment01.Data;
+using Assignment01.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Assignment01.Controllers;
 
-public class EventsController : Controller
-{
+public class EventsController : Controller {
+    private readonly AppDbContext _context;
     public IActionResult ManageEvents() => View();
-    
-    public IActionResult PurchaseTicket() => View();
+
+    [HttpPost]
+    public IActionResult Events(Event events) {
+        if (!ModelState.IsValid) return View(events);
+    }
+
+
+
 }
