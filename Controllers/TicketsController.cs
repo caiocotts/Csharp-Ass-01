9f2322698deb499b2512c349a0e093640f3e57e5
keag42 <keagancam@gmail.com>
2025-10-06 16:22:35 -0400

    ticket purchasing underway (2/5)

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -1,11 +1,22 @@
+using Assignment01.Data;
+using Assignment01.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Assignment01.Controllers;
 
-public class TicketsController : Controller
+public class TicketsController (AppDbContext context) : Controller
 {
-    public IActionResult TicketPurchasing() => View();
+    public IActionResult TicketPurchasing() {
+        var events = context.Events.ToList();
+        return View(events);
+    }
+
+    public IActionResult PurchaseConfirm(int id) {
+        var anEvent = context.Events.Find(id);
+        if (anEvent == null) return NotFound();
+        return View(anEvent);
+    }
+
     
-    public IActionResult PurchaseConfirm() => View();
 
 }
