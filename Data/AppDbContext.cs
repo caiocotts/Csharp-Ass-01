380f2bdcd8bb722fe092e779f5adf5f84435edfe
Caio Cotts <caio@cotts.com.br>
2025-10-15 21:47:33 -0400

    create dummy data when starting the application

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -12,4 +12,37 @@ public class AppDbContext : DbContext
     public DbSet<Event> Events { get; set; }
     public DbSet<Purchase> Purchases { get; set; }
     public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Event>()
+            .HasMany(e => e.Purchases)
+            .WithOne(p => p.Event)
+            .HasForeignKey(p => p.EventId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        var event1Date = new DateTime(2025, 10, 1, 0, 0, 0, DateTimeKind.Utc);
+        var event2Date = new DateTime(2025, 11, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        modelBuilder.Entity<Event>().HasData(
+            new Event
+            {
+                Id = 1,
+                Title = "Event 1",
+                Category = "Workshop",
+                AvailableTickets = 100,
+                EventDate = event1Date,
+                PricePerTicket = 50
+            },
+            new Event
+            {
+                Id = 2,
+                Title = "Event 2",
+                Category = "Concert",
+                AvailableTickets = 100,
+                EventDate = event2Date,
+                PricePerTicket = 0
+            }
+        );
+    }
 }
---
+++ Migrations/20251016013656_AddDummyData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Assignment01.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDummyData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Events",
+                columns: new[] { "Id", "AvailableTickets", "Category", "EventDate", "PricePerTicket", "Title" },
+                values: new object[,]
+                {
+                    { 1, 100, "Workshop", new DateTime(2025, 10, 1, 0, 0, 0, 0, DateTimeKind.Utc), 50.0, "Event 1" },
+                    { 2, 100, "Concert", new DateTime(2025, 11, 1, 0, 0, 0, 0, DateTimeKind.Utc), 0.0, "Event 2" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
