c3cafbb764eb4d2cb542faae6458c6d5aaada8ef
Caio Cotts <caio@cotts.com.br>
2025-12-02 17:44:04 -0500

    more explicit availability filter text

--- ViewModels/ManageEventsViewModel.cs
+++ ViewModels/ManageEventsViewModel.cs
@@ -5,7 +5,7 @@ namespace Assignment01.ViewModels;
 public class ManageEventsViewModel
 {
     private static readonly string[] DefaultCategoryOptions = ["Webinar", "Concert", "Workshop", "Conference"];
-    private static readonly string[] DefaultAvailabilityOptions = ["All", "Available", "Sold Out"];
+    private static readonly string[] DefaultAvailabilityOptions = ["Any Availability", "Available", "Sold Out"];
 
     public IReadOnlyList<Event> Events { get; init; } = Array.Empty<Event>();
     public EventFilterOptions Filters { get; init; } = new();
