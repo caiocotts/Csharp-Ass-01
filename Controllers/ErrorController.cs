a0e56b79eb6edfe282ac29ffb891309821051cf4
keaganPC <keagan.cameron@georgebrown.ca>
2025-12-05 11:39:51 -0500

    error controller

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment01.Controllers;
+
+public class ErrorController : Controller {
+    
+    // handles 404-page stuff
+    [Route("Error/{statusCode}")]
+    public IActionResult HttpStatusCodeHandler(int statusCode)
+    {
+        if (statusCode == 404)
+        {
+            ViewBag.ErrorMessage = "Sorry, due to technical difficulties we cannot load this page right now.";
+                
+            return View("NotFound"); 
+        }
+        
+        return View("Error");
+    }
+
+    // handles all the 500-page stuff
+    [Route("Error/500")]
+    public IActionResult ServerError()
+    {
+        return View("ServerError");
+    }
+}
